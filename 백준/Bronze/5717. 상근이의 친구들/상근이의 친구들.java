02877bbf1c584f730b051ba30cecfa88dc419514
moss_log <ahwlstjd57@gmail.com>
2026-04-24 00:37:30 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 88 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -1,31 +1,12 @@
-import java.util.*;
-import java.io.*;
-
-public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
-	public static void main(String[] args) throws Exception{
-		process();
-	}
-	
-	private static void process() throws Exception{
-        int a, b;
-        while(true){
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            
-            if(a == 0 && b == 0) break;
-            
-            solve(a, b);
-        }
-		
-		System.out.println(sb);
-	}
-	
-	private static void solve(int a, int b) {
-		sb.append(a + b).append("\n");
-	}
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->

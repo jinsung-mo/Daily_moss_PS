279acccc0facc44ca043cb2cb3cffdca025c5ec1
moss_log <ahwlstjd57@gmail.com>
2026-04-24 22:57:24 +0900

    [Bronze IV] Title: 새천년관, Time: 96 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception{
+		process();
+	}
+	
+	private static void process() throws Exception{
+        solve();
+        
+		System.out.println(sb);
+	}
+	
+	private static void solve() {
+		sb.append("ngwankugwan");
+	}
+}

6cd77009cbe6179298e38909aef7c743f9fcb84f
moss_log <ahwlstjd57@gmail.com>
2026-07-09 09:38:36 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        long result = dp(n);
+        
+        return result;
+    }
+    
+    public long dp(int n){
+        long[] dp = new long[n + 1];
+        if(n == 1) return 1;
+        
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}

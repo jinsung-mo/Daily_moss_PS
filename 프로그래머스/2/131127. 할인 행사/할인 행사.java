eb0c4355d0f92080f88b050e81049fdfd883953f
moss_log <ahwlstjd57@gmail.com>
2026-07-11 00:23:28 +0900

    [level 2] Title: 할인 행사, Time: 38.92 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int count = 0;
+        
+        Map<String, Integer> wantMap = new HashMap<>();
+        for(int i = 0; i < want.length; i++){
+            wantMap.put(want[i], number[i]);
+        }
+        
+        for(int i = 0; i <= discount.length - 10; i++){
+            Map<String, Integer> disMap = new HashMap<>();
+            for(int j = i; j < i + 10; j++){
+                disMap.put(discount[j], disMap.getOrDefault(discount[j], 0) + 1);
+            }
+            
+            if(wantMap.equals(disMap)) count++;
+        }
+        
+        return count;
+    }
+}
